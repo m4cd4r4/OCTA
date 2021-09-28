d1ee6b2b201532f0bc380d7dbdf75f7f83ff9757
m4cd4r4 <m4cd4r4@gmail.com>
2021-09-28 11:06:14 +0800

    1st commit on master

---
+++ optovue_auto_printscreen.py
@@ -0,0 +1,28 @@
+"""
+Prerequisites:
+pip install pythonautogui
+https://gadwin.com/printscreen/ 
+
+GadWin is free software that enables capture & saving of screenshots in 2 steps:
+Step 1 - Press 'PrintScrn'
+Step 2 - Press "F2" to confirm
+
+GadWin Options -
+Choose TIFF, BMP, JPEG etc
+
+*If screenshots are labelled out of order (For example, 1, 3, 4, 5 ,2 ,6): change File name Template to "Screen Shot"
+
+**Recommended - Run from Python IDLE, move IDLE until only the toolbar is visible at the bottom of the screen, then press "F5" to run the script
+"""
+
+import time
+import pyautogui
+
+for i in range(304):                        # number of b-scans per volume
+    time.sleep(2)                           # time to minimise any pop-ups & to click near the b-scan scroll bar
+    pyautogui.press('printscreen')
+    time.sleep(.5)                          # a little time for the OS to open the printscreen
+    pyautogui.press('f2')
+    time.sleep(.2)
+    pyautogui.press('down')
+    pyautogui.sleep(.05)
