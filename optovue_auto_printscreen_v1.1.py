185d6a4ee2e587858196a95d879ee060a944b09f
m4cd4r4 <m4cd4r4@gmail.com>
2021-10-05 14:52:11 +0800

    Replaced  reliance on "GadWin" 3rd-party software with pyautogui's built-in screen-grabbing function

--- optovue_auto_printscreen_v1.1.py
+++ optovue_auto_printscreen_v1.1.py
@@ -1,7 +1,7 @@
 """ 
 OptoVue - Optical Coherance Tomography image acquisition machine
 
-This script automates the tedius task of saving each slice of a 304-slice volume
+This script automates the tedius task of screengrabbing each slice of a 304-slice volume
 PyAutoGui allows for the selection of specific regions on-screen, combined with mouse strokes & keyboard combinations
 
 Prerequisites:
@@ -9,7 +9,7 @@ pip install pythonautogui
 
 **Recommended - Run from Python IDLE, move IDLE until only the toolbar is visible at the bottom of the screen, then press "F5" to run the script
 
-v1.1 - Replaced "GadWin" with pyautogui's built-in screenshot capability
+v1.1 - Replaced "GadWin" with pyautogui's built-in screenshot function
 """
 
 import time
